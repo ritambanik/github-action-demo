7f0870b2c11092769e0a55d371a96456a3e2ada6
dt233894 <ritam.banik@sscinc.com>
2025-03-19 16:17:45 -0400

    Fixes as per flake review

--- test/test_1.py
+++ test/test_1.py
@@ -1,10 +1,13 @@
-import os, sys
+import os
+import sys
+from script import addition
+
 
 sys.path.insert(0, os.getcwd())
 
-from script import addition
 
 def test_addition():
     assert addition(1, 2) == 3
     assert addition(5, 5) == 10
     assert addition(10, 10) == 20
+    
--- test/test_2.py
+++ test/test_2.py
@@ -1,8 +1,12 @@
-import os, sys
+import os
+import sys
+from script import addition
+
 
 sys.path.insert(0, os.getcwd())
 
-from script import addition
 
 def test_addition_instance():
     assert isinstance(addition(1, 2), int)
+    
+
